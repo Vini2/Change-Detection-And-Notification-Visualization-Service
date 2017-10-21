b9995602c44c7316d8cc692ec72ebc29d14a4647
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2017-10-21 23:36:21 +0530

    Add dummy landing page and home page

--- visualization-ui/src/app/app.module.ts
+++ visualization-ui/src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
+const routes = [
+  {
+    path: 'home',
+    component: HomePageComponent
+  },
+  {
+    path: '',
+    component: LandingPageComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    LandingPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
